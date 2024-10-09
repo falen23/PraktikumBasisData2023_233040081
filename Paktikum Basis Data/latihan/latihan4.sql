5bb80c91a90451fee87d71b526389524caefd58a
falen23 <146053331+falen23@users.noreply.github.com>
2024-10-09 18:49:57 +0700

    Add files via upload

---
+++ Paktikum Basis Data/latihan/latihan4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    C.custid, 
+    C.companyname, 
+    O.orderid, 
+    O.orderdate 
+FROM 
+    Sales.Customers AS C   
+LEFT JOIN 
+    Sales.Orders AS O     
+ON 
+    C.custid = O.custid
+ORDER BY 
+    C.custid;
+
+
